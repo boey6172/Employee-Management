f243839f5d48b01d5e663d141da947f9e189cae7
Daniel Meynard Mabunga <boey6172@gmail.com>
2024-02-25 16:12:59 +0800

    fix: sql update

--- server/sql/sqlUpdate022524.sql
+++ server/sql/sqlUpdate022524.sql
@@ -1,4 +1,4 @@
 USE `hxp`
 ALTER TABLE `Donors`  ADD `donor_id` VARCHAR(255) NOT NULL  AFTER `id`;
-ALTER TABLE `Donors` CHANGE `donor_id` `donor_id` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
+ALTER TABLE `Donors` CHANGE `donor_id` `donor_id` VARCHAR(255) NULL;
 ALTER TABLE `Donors`  ADD `isRejected` TINYINT NULL  AFTER `validatedDonorSolicitorBy`;
